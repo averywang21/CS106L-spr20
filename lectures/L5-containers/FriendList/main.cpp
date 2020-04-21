8b0227c62a8c67df426c3de9049efce0c165534b
Avery Wang <averywang@Averys-iMac.local>
2020-04-20 18:35:11 -0700

    Final L5 lecture code

--- lectures/L5-containers/FriendList/main.cpp
+++ lectures/L5-containers/FriendList/main.cpp
@@ -22,29 +22,34 @@
     Here is the function prototype you should implement:
     Map<string, Vector<string> > friendList(String filename)
 */
-std::map<std::string, std::set<std::string> > friendList(const std::string& filename) {
-    // solution from CS 106B using Stanford libraries, 
-    // except we won't check the return value of openFile
-    // cause the solution doesn't handle the case when it fails anyways
-    // we also use a set as the value of the map instead of a vector
-    // since what if the same line appears twice in the file?
+std::map<std::string, std::set<std::string> > friendList (const std::string& filename) {
+    // This is the solution to the friendList problem
+    // with one change: instead of a Vector as the key, we use a Set
 
-    // ifstream in;
-    // Vector<string> lines;
-    // openFile(in, filepath)
-    // readEntireFile(in, lines);
+    std::ifstream in;                                       // ifstream in;
+    std::vector<std::string> lines;                         // Vector<string> lines;
 
-    // Map<string, Set<string> > friends;
-    // for (string line: lines) {
-    //     Vector<string> people = stringSplit(line, " ");
-    //     string s1 = people[0];
-    //     string s2 = people[1];
-    //     friends[s1] += s2;
-    //     friends[s2] += s1;
-    // }
-    // return friends;  
+    in.open(filename);   
+    std::string line;                                       // openFile(in, filename)
+    while(std::getline(in, line)) {                         // readEntireFile(in, lines);
+        lines.push_back(line);
+    }  
 
-    return {}; // just here so starter code compiles
+    std::map<std::string, std::set<std::string>> friends;   // Map<string, Set<string> > friends;     
+    for (const auto& line : lines) {                        // for (string line: lines) {
+        std::istringstream iss(line);                       //      Vector<string> people = stringSplit(line, " ");
+        std::string s1, s2;                                 //      string s1 = people[0];
+        iss >> s1 >> s2;                                    //      string s2 = people[1];
+
+        // alternative if you don't like stringstreams
+        // size_t split_index = line.find(" ");
+        // std::string s1 = line.substr(0, split_index);
+        // std::string s2 = line.substr(split_index+1);
+
+        friends[s1].insert(s2);                             //      friends[s1] += s2;
+        friends[s2].insert(s1);                             //      friends[s2] += s1;
+    }                                                       // }
+    return friends;
 }
 
 /*
@@ -59,8 +64,6 @@ std::map<std::string, std::set<std::string> > friendList(const std::string& file
             by token using >> (Anna briefly covered it in L4, but you might want to read more)
  */
 std::map<std::string, std::set<std::string> > betterFriendList(const std::string& filename) {
-
-    
     return {}; // just here so starter code compiles
 }
 
@@ -72,7 +75,7 @@ int main() {
     for (const auto& [name, friends] : answer) {
         std::cout << name << ": {";
         auto last = --friends.end();
-        for (auto iter = friends.begin(); iter != friends.end(); ++iter) {
+        for (auto iter = friends.begin(); iter != last; ++iter) {
             std::cout << *iter << ", ";
         }
         std::cout << *last << "}" << '\n';
