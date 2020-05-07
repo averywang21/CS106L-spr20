7b9b334c7db3eda1221838b7d681e3a26dc4b1b1
aszeng <36085659+aszeng@users.noreply.github.com>
2020-05-06 18:31:48 -0700

    Updated main.cpp with completed code!

--- lectures/L9-summary/main.cpp
+++ lectures/L9-summary/main.cpp
@@ -36,7 +36,78 @@ const vector<string> FEATURE_VEC{"a", "about", "above", "after", "again", "again
                                  ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_",
                                  "`", "{", "|", "}", "~"};
 
+
+string fileToString(ifstream& file) {
+    string ret = "";
+	string line;
+	while (getline(file, line)) {
+		std::transform(line.begin(), line.end(), line.begin(), tolower);	
+		ret += line + " ";
+	}
+    return ret;
+}
+
+int countOccurrences(const string& text, const string& feature) {
+    int count = 0;
+	string toFind = " " + feature + " ";
+	
+	auto curr = text.begin();
+	auto end = text.end();
+	while (curr != end) {
+		auto found = std::search(curr, end, toFind.begin(), toFind.end());
+		if (found == end) break;
+		
+		++count;
+		curr = found + 1;
+	}
+
+    return count;
+}
+
+vector<int> createCountVec(const string& text) {
+    vector<int> result;	
+	for (const string& feature : FEATURE_VEC) {
+		result.push_back(countOccurrences(text, feature));
+	}
+    return result;
+}
+
+int dotProduct(const vector<int>& v1, const vector<int>& v2) {
+	return std::inner_product(v1.begin(), v1.end(), v2.begin(), 0);
+}
+
+double mag(const vector<int>& v) {
+	return std::sqrt(dotProduct(v, v));
+}
+
+// To clarify, this actually represents cos(theta), so a higher return value
+// corresponds to a closer match.
+double angle(const vector<int>& v1, const vector<int>& v2) {
+	return dotProduct(v1, v2) / (mag(v1) * mag(v2));
+}
+
 int main() {
-    // TODO: Implement Stylometry!
-    return 0;
+    // Read in the files and convert file to a single string
+	ifstream hamilton_fs("res/hamilton.txt");
+	ifstream jj_fs("res/jj.txt");
+	ifstream madison_fs("res/madison.txt");
+	ifstream unknown_fs("res/unknown.txt");
+
+	string hamilton_txt = fileToString(hamilton_fs);
+	string jj_txt = fileToString(jj_fs);
+	string madison_txt = fileToString(madison_fs);
+	string unknown_txt = fileToString(unknown_fs);
+	
+	// Create our feature vector
+	vector<int> hamilton_vec = createCountVec(hamilton_txt);
+	vector<int> jj_vec = createCountVec(jj_txt);
+	vector<int> madison_vec = createCountVec(madison_txt);
+	vector<int> unknown_vec = createCountVec(unknown_txt);
+
+	// Compute the "angle" between feature vector of unknown and each of the three authors
+ 	cout << "Similarity - hamilton <-> unknown: " << angle(hamilton_vec, unknown_vec) << endl;
+	cout << "Similarity - jj <-> unknown: " << angle(jj_vec, unknown_vec) << endl;
+	cout << "Similarity - madison <-> unknown: " << angle(madison_vec, unknown_vec) << endl;
+
+	return 0;
 }
