709d49726f149f5b5de483566fd7812376a144e9
aszeng <aszeng@stanford.edu>
2020-05-27 17:55:24 -0700

    Updated with code from lecture

--- lectures/L15-raii/template-vector/main.cpp
+++ lectures/L15-raii/template-vector/main.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <deque>
 #include <algorithm>
+#include <memory>
 using namespace mycollection;
 
 using std::cout;
 using std::endl;
 using std::string;
 
-void badFunc() {
+void badFunc() noexcept {
 	throw std::out_of_range("Whoops");
 }
 
@@ -17,14 +18,22 @@ int main() {
 	vec.push_back("Avery");
 	vec.push_back("Anna");
 	vec.push_back("CS106L");
-	
+
+	//vector<int> heapVec = new vector<int>();
+	//std::unique_ptr<vector<int>> heapUp = std::make_unique<vector<int>>();
+
 	try {
-		string badString = vec.at(3);
+		string badString = vec.at(3); // 3 is out of bounds
 		cout << badString << endl; 
-		//badFunc();
+		badFunc();
 	} catch (const std::exception& e) {
 		cout << e.what() << endl;
 		throw;
 	}
+
+	std::shared_ptr<int> sp1(new int);
+	int* rawPtr = sp1.get();
+	std::shared_ptr<int> sp2(rawPtr); // This is undefined behavior!
+	std::shared_ptr<int> sp3 = sp1;
 }
 
