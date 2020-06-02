d4afcf05f75378955f810abf6de17323117acede
aszeng <aszeng@stanford.edu>
2020-06-01 18:18:21 -0700

    Updated completed code for the BasicThreading program! Leaving MySharedPointer and TicketAgent incomplete because they're great exercises with what we've been learning, but feel free to post on Piazza if you want to check your answer

--- lectures/L16-multithreading/BasicThreading/main.cpp
+++ lectures/L16-multithreading/BasicThreading/main.cpp
@@ -6,11 +6,14 @@
 
 using std::cout;    using std::endl;
 using std::string;  using std::vector;
+using std::thread;
 
 const size_t NUM_THREADS = 10;
-const std::mutex mtx;
+static std::mutex mtx;
 
 void greet(int id) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	std::lock_guard<std::mutex> lg(mtx);
     cout << "Thread " << id << ": Hello world!" << endl;
 }
 
@@ -18,8 +21,15 @@ int main() {
     cout << "Greetings from my threads..." << endl;
 
     // Create threads
+	vector<thread> threads;
+	for (size_t i = 0; i < NUM_THREADS; ++i) {
+		threads.push_back(thread(greet, i));
+	}
 
     // Wait for threads to finish
+	for (std::thread& t: threads) {
+	    t.join();
+	}
 
     cout << "All greetings done!" << endl;
 
